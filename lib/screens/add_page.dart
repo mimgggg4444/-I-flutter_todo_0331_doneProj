9f528db9b924c9d0e658b8db227cca0554a26853
e <minnki19@gmail.com>
2024-03-31 18:54:41 +0900

    Create Todo Task [POST] 2-1,2-2 fin

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -8,6 +12,9 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,21 +25,68 @@ class _AddTodoPageState extends State<AddTodoPage> {
         padding: EdgeInsets.all(20),
         children: [
           TextField(
+            controller: titleController,
             decoration: InputDecoration(hintText: '제목'),
           ),
           TextField(
+            controller: descriptController,
             decoration: InputDecoration(hintText: '제목'),
             keyboardType: TextInputType.multiline,
             minLines: 5,
             maxLines: 8,
           ),
-          SizedBox(height: 20,),
+          SizedBox(
+            height: 20,
+          ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: submitData,
             child: Text('저장'),
           )
         ],
       ),
     );
+  }
+
+  Future<void> submitData() async {
+    final title = titleController.text;
+    final description = descriptController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false,
+    };
+
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final respose = await http.post(
+      uri,
+      body: jsonEncode(body),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (respose.statusCode == 201) {
+      titleController.text='';
+      descriptController.text='';
+      showSuccessMessage('creation success');
+      print('creation success');
+    } else {
+      showErrorMessage('creation failed');
+    }
+  }
+
+  void showSuccessMessage(String message) {
+    final snackBar = SnackBar(content: Text(message));
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+          message,
+      style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
